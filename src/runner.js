00cffb8c8438a1898864cacbd64c1263409e122e
Aaron Smith <aaron.j.smith2019@gmail.com>
2025-08-08 16:05:13 -0700

    Update to use async execa invocation

--- src/runner.js
+++ src/runner.js
@@ -17,7 +17,7 @@ if (!command) {
 
 console.log(` > running ${hook} hook: ${command}`)
 try {
-  execa.sync(command, { shell: true, stdio: 'inherit' })
+  await execa(command, { stdio: 'inherit' })
 } catch (e) {
   process.exit(1)
 }
